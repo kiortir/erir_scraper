221d218f6f549c1907d48ad27cd6bc928e482e71
Michael Plekhanov <kiortir@yandex.ru>
2025-07-06 10:33:29 +0300

    cleanup

--- src/erir_scraper/main.py
+++ src/erir_scraper/main.py
@@ -26,47 +26,6 @@ app.add_middleware(
 )
 
 
-def to_datetime(
-    date_obj: datetime.date, to_end: bool = False
-) -> datetime.datetime:
-    year = date_obj.year
-    month = date_obj.month
-    day = date_obj.day
-    if to_end:
-        hour = 23
-        minute = 59
-        second = 59
-        microsecond = 999
-    else:
-        hour = 0
-        minute = 0
-        second = 0
-        microsecond = 0
-    return datetime.datetime(
-        year, month, day, hour, minute, second, microsecond
-    )
-
-
-async def generator(page_1, filters, repository):
-    with StringIO() as io:
-        writer = csv.DictWriter(
-            io, fieldnames=InvoiceResponseDto.model_fields, delimiter=";"
-        )
-        writer.writeheader()
-        c = [row.model_dump(mode="json") for row in page_1.content]
-        writer.writerows(c)
-        total_pages = page_1.totalPages
-        print(total_pages)
-        for page_idx in range(1, total_pages):
-            page = await repository.get_invoice_list(
-                filters, page=page_idx, page_size=200000
-            )
-            c = [row.model_dump(mode="json") for row in page.content]
-            writer.writerows(c)
-
-        yield io.getvalue().encode("utf-8-sig")
-
-
 @app.post("/export/invoices")
 def export_invoices(
     body: Any = Body(),
